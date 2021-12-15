fb7a634e9d7813300972acbe5b2cc647ad336189
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-15 14:28:00 -0600

    adds inline and annotations tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/AnnotationMocksTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+public class AnnotationMocksTest {
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+    @Mock
+    Map<String, Object> mapMock;
+
+    @Test
+    void testMock(){
+        mapMock.put("keyvalue", "foo");
+    }
+
+}
